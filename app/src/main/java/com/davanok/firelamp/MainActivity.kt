1d37491ab933f9581a7e4eff5ca5ee24518e6fd5
davanok <r9195253857@gmail.com>
2025-09-26 17:58:34 +0300

    initial commit

---
+++ app/src/main/java/com/davanok/firelamp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.davanok.firelamp
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.expressiveLightColorScheme
+import com.davanok.firelamp.ui.Application
+
+class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3ExpressiveApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            val dynamicColorsEnabled =  Build.VERSION.SDK_INT > 31
+            val isDarkColors = isSystemInDarkTheme()
+
+            val colorScheme =
+                if (dynamicColorsEnabled) {
+                    if (isDarkColors) dynamicDarkColorScheme(this)
+                    else dynamicLightColorScheme(this)
+                }
+                else {
+                    if (isDarkColors) darkColorScheme()
+                    else expressiveLightColorScheme()
+                }
+
+            Application(colorScheme = colorScheme)
+        }
+    }
+}
---
+++ app/src/main/java/com/davanok/firelamp/ui/Application.kt
@@ -0,0 +1,18 @@
+package com.davanok.firelamp.ui
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun Application(
+    colorScheme: ColorScheme
+) {
+    MaterialExpressiveTheme(
+        colorScheme = colorScheme
+    ) {
+
+    }
+}
